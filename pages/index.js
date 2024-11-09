998815a43489e738582e278e81b1a0831cbb6718
rspartan-117 <riteksharmachd@gmail.com>
2024-11-09 22:44:44 +0530

    Initial commit

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import {
   AiFillTwitterCircle,
   AiFillLinkedin,
   AiFillYoutube,
+  AiFillGithub,
 } from "react-icons/ai";
 import { BsFillMoonStarsFill } from "react-icons/bs";
 import { useState } from "react";
@@ -17,9 +18,14 @@ import web3 from "../public/web3.png";
 import web4 from "../public/web4.png";
 import web5 from "../public/web5.png";
 import web6 from "../public/web6.png";
+import Link from "next/link";
+import gemini_thumbnail from "../public/gemini_thumbnail.png";
+import quote_thumbnail from "../public/quote_thumbnail.png";
+import youtube_thumbnail from "../public/youtube_thumbnail.png"
+import weather_thumbnail from "../public/weather_thumbnail.png"
 
 export default function Home() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
 
   return (
     <div className={darkMode ? "dark" : ""}>
@@ -31,7 +37,7 @@ export default function Home() {
       <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
         <section className="min-h-screen">
           <nav className="py-10 mb-12 flex justify-between dark:text-white">
-            <h1 className="font-burtons text-xl">developedbyed</h1>
+            <h1 className="font-burtons text-xl">PORTFOLIO WEBSITE</h1>
             <ul className="flex items-center">
               <li>
                 <BsFillMoonStarsFill
@@ -42,7 +48,7 @@ export default function Home() {
               <li>
                 <a
                   className="bg-gradient-to-r from-cyan-500 text- to-teal-500 text-white px-4 py-2 border-none rounded-md ml-8"
-                  href="#"
+                  href="https://drive.google.com/file/d/1ardCJviRSlKovEuEEoIb1g6XAtBEn_zX/view?usp=drivesdk"
                 >
                   Resume
                 </a>
@@ -51,158 +57,143 @@ export default function Home() {
           </nav>
           <div className="text-center p-10 py-10">
             <h2 className="text-5xl py-2 text-teal-600 font-medium dark:text-teal-400 md:text-6xl">
-              Dimitri Marco
+              Ritek Sharma
             </h2>
             <h3 className="text-2xl py-2 dark:text-white md:text-3xl">
-              Developer and designer.
+              Front-End Developer
             </h3>
             <p className="text-md py-5 leading-8 text-gray-800 dark:text-gray-200 max-w-xl mx-auto md:text-xl">
-              Freelancer providing services for programming and design content
-              needs. Join me down below and let's get cracking!
+            Frontend developer with a passion for building beautiful and functional web interfaces.Skilled in crafting engaging and user-friendly interfaces that bring ideas to life. 
+            
             </p>
             <div className="text-5xl flex justify-center gap-16 py-3 text-gray-600 dark:text-gray-400">
               <AiFillTwitterCircle />
-              <AiFillLinkedin />
-              <AiFillYoutube />
+              <Link href="https://www.linkedin.com/in/ritik-sharma-cse/">
+              <AiFillLinkedin className="cursor-pointer"/>
+              </Link>
+              <Link href="https://github.com/rspartan-117">
+              <AiFillGithub  className="cursor-pointer"/>
+              </Link>
             </div>
             <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" />
+              <Image src={deved} layout="fill" objectFit="cover" alt="Icon"/>
             </div>
           </div>
         </section>
         <section>
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Services I offer</h3>
+            <h3 className="text-3xl py-1 dark:text-white">Skills</h3>
+           
             <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
+            I offer a wide range of services, including responsive web design, 
+            interactive front-end development, and optimized code solutions
             </p>
           </div>
           <div className="lg:flex gap-10">
-            <div className="text-center shadow-lg p-10 rounded-xl my-10  dark:bg-white flex-1">
-              <Image src={design} width={100} height={100} />
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-500 flex-1">
+              <Image src={design} width={100} height={100} alt="Icon" />
               <h3 className="text-lg font-medium pt-8 pb-2  ">
-                Beautiful Designs
+              High-Quality Development
               </h3>
               <p className="py-2">
-                Creating elegant designs suited for your needs following core
-                design theory.
+              Building efficient and scalable code to create seamless user experiences.&apos;
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h4 className="py-4 text-teal-500  dark:text-teal-200">Technologies I Work With</h4>
+              <p className="text-gray-900 py-1">HTML & CSS</p>
+              <p className="text-gray-900 py-1">JavaScript/Typescript</p>
+              <p className="text-gray-900 py-1">React</p>
+              <p className="text-gray-900 py-1">NextJs</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={code} width={100} height={100} />
+            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-gray-500  flex-1">
+              <Image src={code} width={100} height={100} alt="Icon" />
               <h3 className="text-lg font-medium pt-8 pb-2 ">
-                Code your dream project
+              User-Centric Experiences
               </h3>
               <p className="py-2">
-                Do you have an idea for your next great website? Let's make it a
-                reality.
+              Designing and developing intuitive interfaces that prioritize user experience and accessibility.
               </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
+              <h4 className="py-4 text-teal-500  dark:text-teal-200">Key Frontend Practices</h4>
+              <p className="text-gray-900 py-1">Responsive Design</p>
+              <p className="text-gray-900 py-1">Accessible Components</p>
+              <p className="text-gray-900 py-1">Interactive Animations</p>
+              <p className="text-gray-900 py-1">Cross-Browser Compatibility</p>
             </div>
-            <div className="text-center shadow-lg p-10 rounded-xl my-10 dark:bg-white flex-1">
-              <Image src={consulting} width={100} height={100} />
-              <h3 className="text-lg font-medium pt-8 pb-2 ">Consulting</h3>
-              <p className="py-2">
-                Are you interested in feedback for your current project? I can
-                give you tips and tricks to level it up.
-              </p>
-              <h4 className="py-4 text-teal-600">Design Tools I Use</h4>
-              <p className="text-gray-800 py-1">Photoshop</p>
-              <p className="text-gray-800 py-1">Illustrator</p>
-              <p className="text-gray-800 py-1">Figma</p>
-              <p className="text-gray-800 py-1">Indesign</p>
-            </div>
+            
           </div>
         </section>
         <section className="py-10">
           <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
+            <h3 className="text-3xl py-1 dark:text-white "> MY PROJECTS</h3>
+           
+           
           </div>
           <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
             <div className="basis-1/3 flex-1 ">
+            <Link href="https://comfy-hummingbird-67bcac.netlify.app">
+            <div>
+            <h1 className="text-xl font-bold mt-4 text-cyan-50 cursor-pointer">Gemini Clone</h1>
               <Image
-                className="rounded-lg object-cover"
+                className="rounded-lg object-contain  cursor-pointer"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web1}
+                src={gemini_thumbnail}
+                alt="Gemini Clone"
+              
+                
               />
+              </div>
+              
+              </Link>
             </div>
             <div className="basis-1/3 flex-1">
+            <Link href="https://capable-donut-8b0645.netlify.app">
+            <div>
+            <h1 className="text-xl font-bold mt-4 text-cyan-50 cursor-pointer">Youtube Clone</h1>
               <Image
-                className="rounded-lg object-cover"
+                className="rounded-lg object-contain  cursor-pointer"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web2}
+                src={youtube_thumbnail}
+                alt="Youtube Clone"
               />
+              </div>
+              </Link>
             </div>
             <div className="basis-1/3 flex-1">
+            <Link href="https://super-sherbet-2c13ad.netlify.app">
+            <div>
+            <h1 className="text-xl font-bold mt-4 text-cyan-50 cursor-pointer">Weather App</h1>
               <Image
-                className="rounded-lg object-cover"
+                className="rounded-lg object-contain  cursor-pointer"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web3}
+                src={weather_thumbnail}
+               alt="Weather App"
               />
+              </div>
+              </Link>
             </div>
             <div className="basis-1/3 flex-1">
+            <h1 className="text-xl font-bold mt-4 text-cyan-50 cursor-pointer">Quote Generator</h1>
+            <Link href="https://fastidious-cocada-889604.netlify.app">
+            <div>
               <Image
-                className="rounded-lg object-cover"
+                className="rounded-lg object-contain  cursor-pointer"
                 width={"100%"}
                 height={"100%"}
                 layout="responsive"
-                src={web4}
+                src={quote_thumbnail}
+                alt="Quote_Generator"
+                
               />
+              </div>
+              </Link>
             </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web5}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web6}
-              />
-            </div>
+            
+           
           </div>
         </section>
       </main>
