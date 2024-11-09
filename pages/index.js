fa1ef7ebca8fef5f7816494ff1c2a288d60132e1
rspartan-117 <riteksharmachd@gmail.com>
2024-11-09 23:13:25 +0530

    changed dark mode default

--- pages/index.js
+++ pages/index.js
@@ -75,9 +75,7 @@ export default function Home() {
               <AiFillGithub  className="cursor-pointer"/>
               </Link>
             </div>
-            <div className="mx-auto bg-gradient-to-b from-teal-500 rounded-full w-80 h-80 relative overflow-hidden mt-20 md:h-96 md:w-96">
-              <Image src={deved} layout="fill" objectFit="cover" alt="Icon"/>
-            </div>
+           
           </div>
         </section>
         <section>
